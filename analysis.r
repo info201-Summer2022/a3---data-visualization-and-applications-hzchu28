e1c540cc1a26618e158d6cc5d37a36b5bff1eda8
RitaHwang <hzchu28@gmail.com>
2022-08-14 17:44:28 -0700

    analysis.r added

---
+++ analysis.r
@@ -0,0 +1,10 @@
+incarceration_df <- read.csv("incarceration_trends.csv")
+View(incarceration_df)
+
+#CA California, WA Washington, AZ Arizona, NM New Mexico, TX Texas, FL Florida, OH Ohio, PA Pennsylvania, IL illinois, NY New York
+
+#Answer questions
+#1 What is the average value of my variable across all the counties (in the current year)?
+#2 Where is my variable the highest / lowest?
+#3 How much has my variable change over the last N years?
+
